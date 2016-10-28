8665fa8e9ac2e790263e2f856dca28c16d4f2cb3
Gerardo Cordero <cordero.gerard@gmail.com>
2016-10-28 09:53:10 -0400

    Add the packages we use in TobMaps and test the menu

--- client/views/common/navigation.js
+++ client/views/common/navigation.js
@@ -1,15 +1,15 @@
-Template.navigation.rendered = function(){
+Template.navigation.onRendered = function(){
 
-    // Initialize metisMenu
-    $('#side-menu').metisMenu();
+		// Initialize metisMenu
+		$('#side-menu').metisMenu();
 
-};
+	};
 
 // Used only on OffCanvas layout
 Template.navigation.events({
 
-    'click .close-canvas-menu' : function(){
-        $('body').toggleClass("mini-navbar");
-    }
+	'click .close-canvas-menu' : function(){
+		$('body').toggleClass("mini-navbar");
+	}
 
 });
